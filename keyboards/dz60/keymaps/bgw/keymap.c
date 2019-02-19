cedb74a5c20f1200ab89982812032286fe85ba05
Benjamin Woodruff <github@benjam.info>
2019-02-18 21:02:21 -0800

    [bgw] Pass through most modifiers, add emoji layer

--- keyboards/dz60/keymaps/bgw/keymap.c
+++ keyboards/dz60/keymaps/bgw/keymap.c
@@ -7,6 +7,7 @@ enum layers {
   _PN,
   _FP,
   _CL,
+  _EM,
 };
 
 enum custom_keycodes {
@@ -25,8 +26,34 @@ enum custom_keycodes {
   CA_RGHT,
   // prints the version of the firmare
   VRSN,
+  // Multi-character emoji
+  E_SHRUG,  // https://emojipedia.org/shrug/
+  E_BMOJI,  // B button (meme)
+  E_TFLIP,  // Table flip
+  E_FFLIP,  // Facebook flip
+  E_TDOWN,  // Setting table down
   // This must come last
   DYNAMIC_MACRO_RANGE,
+};
+
+enum unicode_names {
+  ANGR,
+  FIRE,
+  THIK,
+  CRY,
+  UPDN,
+  POOP,
+  _100,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+  [ANGR] = 0x1F620,  // https://emojipedia.org/angry-face/
+  [FIRE] = 0x1F525,  // https://emojipedia.org/fire/
+  [THIK] = 0x1F914,  // https://emojipedia.org/thinking-face/
+  [CRY]  = 0x1F622,  // https://emojipedia.org/crying-face/
+  [UPDN] = 0x1F643,  // https://emojipedia.org/upside-down-face/
+  [POOP] = 0x1F4A9,  // https://emojipedia.org/pile-of-poo/
+  [_100] = 0x1F4AF,  // https://emojipedia.org/hundred-points-symbol/
 };
 
 // Must come after custom_keycodes
@@ -38,6 +65,10 @@ enum custom_keycodes {
 // SEND_MACRO is just SEND_STRING with a delay between each keypress
 #define MACRO_TIMER 5
 #define SEND_MACRO(str) send_string_with_delay_P(PSTR(str), MACRO_TIMER)
+
+void matrix_init_user(void) {
+  set_unicode_input_mode(UC_LNX);
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (!process_record_dynamic_macro(keycode, record)) {
@@ -87,6 +118,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         ", Built on: " QMK_BUILDDATE
       );
       break;
+    case E_SHRUG:
+      send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF");
+      break;
+    case E_BMOJI:
+      send_unicode_hex_string("1F171 FE0F");
+      break;
+    case E_TFLIP:
+      send_unicode_hex_string(
+        "0028 256F 00B0 25A1 00B0 FF09 256F FE35 0020 253B 2501 253B"
+      );
+      break;
+    case E_FFLIP:
+      send_unicode_hex_string(
+        "0028 256F 00B0 25A1 00B0 0029 256F FE35 0020 029E 006F 006F 0071 01DD "
+        "0254 0250 2132"
+      );
+      break;
+    case E_TDOWN:
+      send_unicode_hex_string(
+        "252C 2500 252C 30CE 0028 0020 00BA 0020 005F 0020 00BA 30CE 0029"
+      );
+      break;
     default:
       return true;
   }
@@ -97,42 +150,50 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Base/default layer
-	[_BA] = LAYOUT_60_ansi(
-		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
-		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-		MO(_CL),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, MO(_FN),          MO(_PN), KC_GRV),
+  [_BA] = LAYOUT_60_ansi(
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
+    KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+    MO(_CL),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+    KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, MO(_FN),          MO(_PN), KC_GRV),
 
   // Fn Layer: WASD, F1-F12, Backlight
-	[_FN] = LAYOUT_60_ansi(
-		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
-		XXXXXXX,          XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
-		XXXXXXX,          KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_HOME, KC_PGUP, XXXXXXX,
-		XXXXXXX,          XXXXXXX, XXXXXXX, BL_DEC,  BL_TOGG, BL_INC,  BL_BRTG, XXXXXXX, KC_DEL,  KC_END,  KC_PGDN,          XXXXXXX,
-		XXXXXXX, XXXXXXX,          XXXXXXX,                   XXXXXXX,                            XXXXXXX, XXXXXXX,          MO(_FP), XXXXXXX),
+  [_FN] = LAYOUT_60_ansi(
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
+    _______,          XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
+    XXXXXXX,          KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_HOME, KC_PGUP, _______,
+    _______,          XXXXXXX, XXXXXXX, BL_DEC,  BL_TOGG, BL_INC,  BL_BRTG, XXXXXXX, KC_DEL,  KC_END,  KC_PGDN,          _______,
+    _______, _______,          _______,                   _______,                            _______, XXXXXXX,          MO(_FP), XXXXXXX),
 
   // Pn Layer: Media, RGB, Window snapping
-	[_PN] = LAYOUT_60_ansi(
-		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
-		XXXXXXX,          XXXXXXX, SP_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CA_MPLY, CA_MPRV, CA_MNXT, XXXXXXX,
-		XXXXXXX,          SP_LEFT, SP_DOWN, SP_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX,
-		XXXXXXX,          XXXXXXX, XXXXXXX, RGB_VAD, RGB_TOG, RGB_VAI, RGB_MOD, XXXXXXX, RGB_SPD, RGB_SPI, XXXXXXX,          XXXXXXX,
-		XXXXXXX, XXXXXXX,          XXXXXXX,                   XXXXXXX,                            XXXXXXX, MO(_FP),          XXXXXXX, XXXXXXX),
+  [_PN] = LAYOUT_60_ansi(
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+    _______,          XXXXXXX, SP_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CA_MPLY, CA_MPRV, CA_MNXT, XXXXXXX,
+    XXXXXXX,          SP_LEFT, SP_DOWN, SP_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, _______,
+    _______,          XXXXXXX, XXXXXXX, RGB_VAD, RGB_TOG, RGB_VAI, RGB_MOD, XXXXXXX, RGB_SPD, RGB_SPI, XXXXXXX,          _______,
+    _______, _______,          _______,                   _______,                            _______, MO(_FP),          XXXXXXX, XXXXXXX),
 
   // Fn+Pn Layer: Workspace switching, macros
   [_FP] = LAYOUT_60_ansi(
-		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
-		XXXXXXX,          XXXXXXX, CA_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		XXXXXXX,          CA_LEFT, CA_DOWN, CA_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
-		XXXXXXX, XXXXXXX,          XXXXXXX,                   XXXXXXX,                            XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX),
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+    _______,          XXXXXXX, CA_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,          CA_LEFT, CA_DOWN, CA_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          _______,
+    _______, _______,          _______,                   _______,                            MO(_EM), XXXXXXX,          XXXXXXX, XXXXXXX),
 
   // Capslock Layer: Numpad, macros
-	[_CL] = LAYOUT_60_ansi(
-		VRSN,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PAST, XXXXXXX, XXXXXXX, KC_PMNS, KC_PPLS,          XXXXXXX,
-		XXXXXXX,          DYN_REC, DYN_STP, DYN_PLY, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_KP_7, KC_KP_8, KC_KP_9, KC_PSLS,
-		XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_KP_4, KC_KP_5, KC_KP_6, KC_PENT,
-		XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_KP_1, KC_KP_2, KC_KP_3,          XXXXXXX,
-		XXXXXXX, XXXXXXX,          XXXXXXX,                   XXXXXXX,                            KC_KP_0, XXXXXXX,          XXXXXXX, KC_PDOT),
+  [_CL] = LAYOUT_60_ansi(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+    _______,          DYN_REC, DYN_STP, DYN_PLY, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, _______,
+    _______,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          _______,
+    _______, _______,          _______,                   _______,                            _______, XXXXXXX,          XXXXXXX, XXXXXXX),
+
+  // emoji layer
+  [_EM] = LAYOUT_60_ansi(
+    VRSN,    X(_100), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+    XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, X(THIK), XXXXXXX, X(UPDN), XXXXXXX, E_FFLIP, X(POOP), XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,          X(ANGR), E_SHRUG, E_TDOWN, X(FIRE), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, E_TFLIP, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,          XXXXXXX, XXXXXXX, X(CRY),  XXXXXXX, E_BMOJI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+    XXXXXXX, XXXXXXX,          XXXXXXX,                   XXXXXXX,                            XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX),
 };
